7a7584349089bf4375f2fc5c7a3729e27bdaf43f
Devz-0047 <devendersingh00747@gmail.com>
2024-11-25 22:56:48 +0530

    Added Readme.md

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,17 +1,8 @@
+from flask import Blueprint, render_template, url_for, flash, redirect
+from flaskblog.forms import RegistrationForm, LoginForm
 
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-from models import User, Post
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '07f1441e22be6d53f289ec35f235fe6c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Corrected key name
-db = SQLAlchemy(app)
+main = Blueprint('main', __name__)
 
-# User model
-
-
-# Example posts for testing
 posts = [
     {
         'author': "Deven Grey",
@@ -27,36 +18,30 @@ posts = [
     }
 ]
 
-# Routes
-@app.route("/")
-@app.route("/home")
-def home():  # Fixed method name from 'Home' to 'home' to match URL
+@main.route("/")
+@main.route("/home")
+def home():
     return render_template('home.html', posts=posts)
 
-@app.route("/about")
+@main.route("/about")
 def about():
     return render_template('about.html', title="About")
 
-@app.route("/register", methods=['GET', 'POST'])
+@main.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))  # Fixed typo 'Hfome' to 'home'
+        return redirect(url_for('main.home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login", methods=['GET', 'POST'])
+@main.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com':  # Basic admin check
+        if form.email.data == 'admin@blog.com':
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check your email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all() 
-    app.run(debug=True)
